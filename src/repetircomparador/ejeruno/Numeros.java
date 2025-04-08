fa8ef9b8bca9dd319dad8dd20099903f318d0ec9
gs.villanueva <gs.villanueva@106-29.info2>
2025-04-08 13:36:43 +0200

    Empeazndo comparadores de nuevo

---
+++ src/repetircomparador/ejeruno/Numeros.java
@@ -0,0 +1,11 @@
+package repetircomparador.ejeruno;
+
+import java.util.Comparator;
+
+public class Numeros implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		return o2-o1;
+	}
+}
---
+++ src/repetircomparador/ejeruno/Principal.java
@@ -0,0 +1,28 @@
+package repetircomparador.ejeruno;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Integer[] numeros = new Integer[20];
+		
+		Random rand = new Random();
+		
+		for(int i=0; i<numeros.length; i++) {
+			numeros[i] = rand.nextInt(1, 101);
+		}
+
+		System.out.println(Arrays.toString(numeros));
+		
+		Arrays.sort(numeros);
+		System.out.println(Arrays.toString(numeros));
+		
+		Arrays.sort(numeros, new Numeros());
+		System.out.println(Arrays.toString(numeros));
+		
+	}
+
+}
