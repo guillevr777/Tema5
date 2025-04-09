d3e1bb376c674a39307b7f528f34b28d9470c1c7
guillevr777 <gs.villanueva@iesnervion.es>
2025-04-09 20:01:27 +0200

    x

--- src/examenes/ex1/Principal.java
+++ src/examenes/ex1/Principal.java
@@ -2,24 +2,33 @@ package examenes.ex1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Principal {
+	
+	static Scanner reader = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		List<Mando> mandos = new ArrayList<Mando>();
 		boolean salir = false;
 		Mando m1 = null;
 		Mando m2 = null;
-		Mando m3 = null;
+		Aspiradora m3 = null;
 		Mando m4 = null;
 		Mando m5 = null;
+		String modelo;
+		
+		Comparator<Mando> mando = (a,b) -> { return a.getModelo().compareTo(b.getModelo());};
 		
 		do {
+//			System.out.println("Dime el modelo");
+//			modelo = reader.nextLine();
 			try {
-				m1 = new Mando("LG1", 10, 5, 10);
+				m1 = new Mando("sp", 10, 5, 10);
 				m2 = new Mando("LG2", 10, 5, 5);
-				m3 = new Mando("LG3", 10, 5, 3.5);
+				m3 = new Aspiradora("asp", 10, 5, 3.5);
 				m4 = new Mando("LG4", 10, 5, 23);
 				m5 = new Mando("LG5", 10, 5, 20.1);
 				salir = true;
